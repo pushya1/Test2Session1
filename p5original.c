27320141e28e5560d8ef1164b125d8714c904c94
pushya1 <86916163+pushya1@users.noreply.github.com>
2022-03-31 04:09:24 +0000

    all the prime numbers between erotosthenes-sieve method

--- p5original.c
+++ p5original.c
@@ -44,3 +44,59 @@ int main()
     n=input_array_size();
     erotosthenes_sieve(n);
 }
+
+
+/*
+
+#include <stdio.h>
+
+int input_n()
+{
+    int n;
+    printf("Enter the number n:\n");
+    scanf("%d",&n);
+    return n;
+}
+
+void init_array(int n,int a[n])
+{   
+    a[0]=0;
+    a[1]=0;
+    for(int i=2;i<=n;i++)
+    {
+        a[i]=i;
+    }
+
+}
+
+void erotosthenes_sieve(int n,int a[n])
+{
+    for(int i=2;i<n;i++)
+    {
+        for(int k=i+i;k<n;k+=i)
+        {
+            a[k]=0;
+        }
+    }
+}
+
+void output(int n,int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0)
+        continue;
+        printf("%d\t",a[i]);
+    }
+}
+
+int main()
+{
+    int n;
+    n=input_n();
+    int a[n];
+    init_array(n,a);
+    erotosthenes_sieve(n,a);
+    output(n,a);
+}
+*/
